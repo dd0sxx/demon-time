7907c813ed5f260fa1415c7a3678dcad8a02e6f6
dd0sxx <contacttheotelonis@gmail.com>
2022-06-04 12:46:57 -0600

    added erc721a

---
+++ src/DemonTime.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "ERC721A/ERC721A.sol";
+
+contract DemonTime is ERC721A {
+
+    constructor () ERC721A ("Demon Time", "DT") {
+
+    }
+
+}
