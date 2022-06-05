2886534d50b4330234bfdcb27af38761ef248ddd
dd0sxx <contacttheotelonis@gmail.com>
2022-06-05 15:31:54 -0600

    max supply

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -13,4 +13,21 @@ contract ContractTest is Test {
         address L = DT.ownerOf(499);
         assertEq(L, 0x8b5B9497e096ee6FfD6041D1Db37a2ac2b41AB0d);
     }
+
+    function testMaxSup() public {
+        // will fail unless you comment out line 35 in DemonTime.sol
+        for (uint i; i < 4500; i++) {
+            DT.summonDemon();
+        }
+        uint ts = DT.totalSupply();
+        assertEq(ts, 5000);
+    }
+
+    function testFailMaxSup() public {
+        // will fail for wrogn reasons unless you comment out line 35 in DemonTime.sol
+        for (uint i; i < 4501; i++) {
+            DT.summonDemon();
+        }
+
+    }
 }
