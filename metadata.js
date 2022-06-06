8a7e631ce88e8003893c3862f0dfbb955eb53b8d
dd0sxx <contacttheotelonis@gmail.com>
2022-06-05 18:07:33 -0600

    0 indexed

--- metadata.js
+++ metadata.js
@@ -4,15 +4,16 @@ const path = '../../Downloads/metadata';
 
 async function run () {
     const files = await fs.readdir(path);
-    for (let i = 1; i < files.length; i++){
+    for (let i = 0; i < 5000; i++){
         let json = await fs.readFile(`${path}/${i}.json`);
         let data = JSON.parse(json);
         data.name = `Demon Time #${i}`;
-        data.image = `ipfs://QmXWDXUXQpaBhD2wsG64YTSYgfEukBgoLE8LHoqU7kGX8V/${i}.webp`
+        data.image = `ipfs://QmZv913jKtYaCdvbnzudZh2Kzfi4SrQrcDxaHWTgFCEbsr/${i}.webp`
         delete data.compiler;
         delete data.properties;
         delete data.external_url;
         let newJSON = JSON.stringify(data);
+        console.log(i)
         await fs.writeFile(`${path}/${i}.json`, newJSON)
     }
 
